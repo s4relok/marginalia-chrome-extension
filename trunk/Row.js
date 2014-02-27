92230df469ff06fa3d8f5ace41443993429674dd
s4relok <s4relok@812a55b2-132e-4332-ac5f-359dd28bf652>
2014-02-27 19:27:12 +0000

    1.4

--- trunk/Row.js
+++ trunk/Row.js
@@ -7,12 +7,38 @@ pb.Row = function(entry) {
     this.urlRowName = this.getValue(entry, options['urlRowName']);
     this.captionRowName = this.getValue(entry, options['captionRowName']);
     this.textRowName = this.getValue(entry, options['textRowName']);
+    //this.urloranotherkey = this.getValue(entry, "urloranotherkey");
+    var s = this.getValue(entry, "lastuse");
+    if(s != null){
+        s = s.replace('"', '');
+        s = s.replace('"', '');
+        this.lastuse = s;
+    }
+    this.idURL = this.getJustValue(entry, "id");
+    this.editURL = this.getEditURL(entry);
+    this.entry = entry.toString();
 };
 
 pb.Row.prototype.getValue = function (entry, name){
     var element = entry.getElementsByTagNameNS('http://schemas.google.com/spreadsheets/2006/extended', name)[0];
+    if(element && element.childNodes.length > 0){
+        return element.childNodes[0].nodeValue;
+    }
+    return null;
+};
+
+pb.Row.prototype.getJustValue = function (entry, name){
+    var element = entry.getElementsByTagName(name)[0];
     if(element){
         return element.childNodes[0].nodeValue;
+    }
+    return null;
+};
+
+pb.Row.prototype.getEditURL = function (entry){
+    var element = entry.getElementsByTagName("link")[1];
+    if(element){
+        return element.getAttribute("href");
     }
     return null;
 };
